c087851774317d28a5b6d6bf32ce9e6b498556de
ConnectShubham1 <Connectshubham815@gmail.com>
2024-07-02 18:52:16 +0530

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        GamePanel gamePanel = new GamePanel();
+        frame.getContentPane().add(gamePanel);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        gamePanel.startGame();
+    }
+}
